358d1f1a07156118918317048fca5f128bcb71ec
cerh2801 <73728136+cerh2801@users.noreply.github.com>
2021-06-23 22:21:30 -0500

    Add files via upload

---
+++ 01_04/test/TestSample.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+public class TestSample  {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\driver\\driver\\chromedriver.exe");
+
+        // Create new instance of ChromeDriver
+        WebDriver driver = new ChromeDriver();
+
+        // And now use this to visit Google
+        driver.get("http://www.google.com");
+
+        // Find the text input element by its name
+        WebElement element = driver.findElement(By.name("q"));
+
+        // Enter something to search for
+        element.sendKeys("Cheese!");
+
+        // Now submit the form
+        element.submit();
+
+        //Close the browser
+        driver.quit();
+    }
+}
+
+
